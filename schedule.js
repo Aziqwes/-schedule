71b53b3e117ec6063260732f37c81aa07a01f849
Nazar Ashirov <newnewqwes@gmail.com>
2020-11-07 19:07:14 +0300

    add functionality

--- schedule.js
+++ schedule.js
@@ -13,36 +13,51 @@ for (let i = 0; i < scheduleText.length; i++) {
 }
 scheduleText[theDay].classList.add('_active');
 
-switch (theDay) {
-    case 0:
-        dayTitleAdd = 'сегодня воскресенье';
-        break;
-    case 1:
-        dayTitleAdd = 'сегодня понедельник';
-        break;
-    case 2:
-        dayTitleAdd = 'сегодня вторник';
-        break;
-    case 3:
-        dayTitleAdd = 'сегодня среда';
-        break;
-    case 4:
-        dayTitleAdd = 'сегодня четверг';
-        break;
-    case 5:
-        dayTitleAdd = 'сегодня пятница';
-        break;
-    case 6:
-        dayTitleAdd = 'сегодня суббота';
-        break;
-}
-dayTitle.innerHTML = dayTitleAdd;
+const switchDay = (day) => {
+    switch (day) {
+        case 0:
+            dayTitleAdd = 'сегодня воскресенье';
+            break;
+        case 1:
+            dayTitleAdd = 'сегодня понедельник';
+            break;
+        case 2:
+            dayTitleAdd = 'сегодня вторник';
+            break;
+        case 3:
+            dayTitleAdd = 'сегодня среда';
+            break;
+        case 4:
+            dayTitleAdd = 'сегодня четверг';
+            break;
+        case 5:
+            dayTitleAdd = 'сегодня пятница';
+            break;
+        case 6:
+            dayTitleAdd = 'сегодня суббота';
+            break;
+    }
+    dayTitle.innerHTML = dayTitleAdd;
+};
+switchDay(theDay);
+
+let caunt = theDay;
 
 buttonBack.addEventListener('click', () => {
-    alert('Нажал кнопку назад! Оброботай этот запрос в JS строка 42');
-    // Писать логику здесь
+    for (let i = 0; i < scheduleText.length; i++) {
+        scheduleText[i].classList.remove('_active');
+        scheduleText[i].classList.add('_hidden');
+    }
+    caunt <= 0 && (caunt = caunt + 7);
+    scheduleText[--caunt].classList.add('_active');
+    switchDay(caunt);
 });
 buttonNext.addEventListener('click', () => {
-    alert('Нажал кнопку Вперед! Оброботай этот запрос в JS строка 45');
-    // Писать логику здесь
+    for (let i = 0; i < scheduleText.length; i++) {
+        scheduleText[i].classList.remove('_active');
+        scheduleText[i].classList.add('_hidden');
+    }
+    caunt >= 6 && (caunt = caunt - 7);
+    scheduleText[++caunt].classList.add('_active');
+    switchDay(caunt);
 });
